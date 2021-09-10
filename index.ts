531c7bd984f14fe9a979b41d1e5b7171b5e2a83b
Anwesh43 <anweshthecool0@gmail.com>
2021-09-10 16:12:27 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -24,3 +24,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSeparateAfterRotate(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(0, (h / 3) * (1 - sc2) + (h * 0.5) * sc4)
+            context.rotate(sc3 * Math.PI / 2)
+            DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLSARNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSeparateAfterRotate(context, scale)
+    }
+}
