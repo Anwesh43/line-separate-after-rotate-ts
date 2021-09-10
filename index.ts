f1814c3935ec298ce461cd798bec78189983a812
Anwesh43 <anweshthecool0@gmail.com>
2021-09-10 16:21:52 +0530

    adding LSARNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,44 @@ class Animator {
         }
     }
 }
+
+class LSARNode {
+    prev : LSARNode 
+    next : LSARNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSARNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSARNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LSARNode {
+        var curr : LSARNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
